0f5e07962f69b467662f86af6c9266894ea0359d
paulkenrick <pkenrick@gmail.com>
2018-01-21 12:03:57 +0000

    Updating sockets config for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://chat-trial-with-sockets.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://chat-trial-with-sockets.herokuapp.com', 'http://chat-trial-with-sockets.herokuapp.com']
+
 end
